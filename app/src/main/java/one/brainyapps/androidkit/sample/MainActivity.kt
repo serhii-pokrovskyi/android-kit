2557ed8738a411d6428178be220bf6ab330cb10b
Serhii Pokrovskyi <10260795+serhii-pokrovskyi@users.noreply.github.com>
2019-09-27 23:13:10 +0300

    initial sample app added

---
+++ app/src/main/java/one/brainyapps/androidkit/sample/MainActivity.kt
@@ -0,0 +1,20 @@
+/*
+ * Developed by Serhii Pokrovskyi
+ * e-mail: pokrovskyi.dev@gmail.com
+ * Last modified: 9/27/19 11:13 PM
+ * Copyright (c) 2019
+ * All rights reserved
+ */
+
+package one.brainyapps.androidkit.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
