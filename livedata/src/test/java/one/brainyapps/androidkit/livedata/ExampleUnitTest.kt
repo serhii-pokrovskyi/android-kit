5a61ab46c2f62b5d34b2c278d59430fb2f3fd6d8
Serhii Pokrovskyi <10260795+serhii-pokrovskyi@users.noreply.github.com>
2019-09-28 11:23:46 +0300

    livedata initial added

---
+++ livedata/src/test/java/one/brainyapps/androidkit/livedata/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+/*
+ * Developed by Serhii Pokrovskyi
+ * e-mail: pokrovskyi.dev@gmail.com
+ * Last modified: 9/28/19 11:23 AM
+ * Copyright (c) 2019
+ * All rights reserved
+ */
+
+package one.brainyapps.androidkit.livedata
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
