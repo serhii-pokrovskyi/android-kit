a4949231ac406124578b326b99d9bd1342bd6c17
Serhii Pokrovskyi <10260795+serhii-pokrovskyi@users.noreply.github.com>
2019-09-28 11:39:55 +0300

    single event and single call added

---
+++ livedata/src/main/java/one/brainyapps/androidkit/livedata/SingleCall.kt
@@ -0,0 +1,58 @@
+/*
+ * Developed by Serhii Pokrovskyi
+ * e-mail: pokrovskyi.dev@gmail.com
+ * Last modified: 9/28/19 11:37 AM
+ * Copyright (c) 2019
+ * All rights reserved
+ */
+
+package one.brainyapps.androidkit.livedata
+
+import androidx.annotation.MainThread
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * Just a single call.
+ * WARNING: Make sure that you have only one observer - because only one will be called.
+ * And there is no way to know witch one will.
+ */
+class SingleCall : LiveData<Void>() {
+
+    private val mPending = AtomicBoolean(false)
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in Void>) {
+        super.observe(owner, Observer { v ->
+            if (mPending.compareAndSet(true, false)) {
+                observer.onChanged(v)
+            }
+        })
+    }
+
+    @MainThread
+    override fun setValue(value: Void?) {
+        mPending.set(true)
+        super.setValue(value)
+    }
+
+    /**
+     * Makes a single call to observer/
+     * Use only from main thread
+     */
+    @MainThread
+    fun call() {
+        value = null
+    }
+
+    /**
+     * Makes a single call to observer/
+     * Can be used from main and background threads
+     */
+    @WorkerThread
+    fun postCall() {
+        postValue(null)
+    }
+}
