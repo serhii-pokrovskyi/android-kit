57e47f9236a1d46a9808bbf1171a9d9a774aa37a
Serhii Pokrovskyi <10260795+serhii-pokrovskyi@users.noreply.github.com>
2019-10-02 22:35:17 +0300

    debug tag fixed

--- logger/src/main/java/one/brainyapps/androidkit/logger/DebugGalaxy.kt
+++ logger/src/main/java/one/brainyapps/androidkit/logger/DebugGalaxy.kt
@@ -1,19 +1,39 @@
 /*
  * Developed by Serhii Pokrovskyi
  * e-mail: pokrovskyi.dev@gmail.com
- * Last modified: 9/30/19 11:26 PM
+ * Last modified: 10/2/19 10:34 PM
  * Copyright (c) 2019
  * All rights reserved
  */
 
 package one.brainyapps.androidkit.logger
 
+import android.os.Build
+
 class DebugGalaxy : Galaxy() {
 
     //todo: think about some ignore list
-    val ignore = listOf("q", "w")
+    val ignore = arrayListOf(
+        Hubble::class.java.name,
+        Galaxy::class.java.name,
+        DebugGalaxy::class.java.name
+    )
 
     override val tag: String?
-        get() = Throwable().stackTrace.first().className
+        get() = Throwable().stackTrace.first {
+            it.className !in ignore
+        }.let {
+            var tag = it.className.substringAfterLast('.')
+
+            // length check
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                if (tag.length > 23) {
+                    // making it shorter
+                    tag.substring(0, 23)
+                }
+            }
+
+            return@let tag
+        }
 
 }
