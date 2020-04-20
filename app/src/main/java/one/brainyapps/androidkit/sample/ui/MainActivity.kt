1763e0a66bb5b8d97d1b89bb8c756485cb9931b2
Serhii Pokrovskyi <10260795+serhii-pokrovskyi@users.noreply.github.com>
2020-04-20 17:00:05 +0300

    project refactored

--- app/src/main/java/one/brainyapps/androidkit/sample/ui/MainActivity.kt
+++ app/src/main/java/one/brainyapps/androidkit/sample/ui/MainActivity.kt
@@ -1,8 +1,8 @@
 /*
  * Developed by Serhii Pokrovskyi
  * e-mail: pokrovskyi.dev@gmail.com
- * Last modified: 10/2/19 10:12 PM
- * Copyright (c) 2019
+ * Last modified: 4/20/20 4:56 PM
+ * Copyright (c) 2020
  * All rights reserved
  */
 
@@ -10,7 +10,6 @@ package one.brainyapps.androidkit.sample.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import one.brainyapps.androidkit.logger.Hubble
 import one.brainyapps.androidkit.sample.R
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +17,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Hubble.i("test log message")
     }
 }
