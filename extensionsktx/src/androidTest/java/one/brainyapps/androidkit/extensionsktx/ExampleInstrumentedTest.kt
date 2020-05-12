71807bc3bf8b2ccb2d20e58f809f7242170a6b21
Serhii Pokrovskyi <10260795+serhii-pokrovskyi@users.noreply.github.com>
2020-05-12 11:48:53 +0300

    new extension module added

---
+++ extensionsktx/src/androidTest/java/one/brainyapps/androidkit/extensionsktx/ExampleInstrumentedTest.kt
@@ -0,0 +1,30 @@
+/*
+ * Developed by Serhii Pokrovskyi
+ * e-mail: serg.pokrovskyi@gmail.com
+ * Last modified: 5/12/20 11:48 AM
+ * Copyright (c) 2020
+ * All rights reserved
+ */
+
+package one.brainyapps.androidkit.extensionsktx
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("one.brainyapps.androidkit.extensionsktx.test", appContext.packageName)
+    }
+}
